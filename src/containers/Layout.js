3aa7cd760d797f250928733e4dd3c7cbe6619c51
Anirudh906 <40207836+Anirudh906@users.noreply.github.com>
2021-04-22 16:33:57 +0530

    modify template

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -12,7 +12,7 @@ import {
 } from "semantic-ui-react";
 import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { logout } from "../store/actions/auth";
+
 
 class CustomLayout extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -2,27 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore, compose, applyMiddleware, combineReducers } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import Test from "./Test"
 
 
-import authReducer from "./store/reducers/auth";
-
-const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const rootReducer = combineReducers({
-  auth: authReducer
-});
-
-const store = createStore(rootReducer, composeEnhances(applyMiddleware(thunk)));
-
-const app = (
-  <Provider store={store}>
-   <Test/>
-  </Provider>
-);
-
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();

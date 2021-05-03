9568ec65dbb9e08035e2093999fb2e595c4508dc
Jay Prakash <unijay12@gmail.com>
2021-05-04 01:59:56 +0530

    docker-compose updated and postgress removed

--- RobotixWeb/settings.py
+++ RobotixWeb/settings.py
@@ -108,26 +108,26 @@ if DEBUG:
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
-        }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'robotixdb3',
-            'USER': 'robot',
-            'PASSWORD' :'django',
-            'HOST' : 'localhost',
-            'PORT' : ''
-
-        }
-
+# if DEBUG:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'robotixdb3',
+#             'USER': 'robot',
+#             'PASSWORD' :'django',
+#             'HOST' : 'localhost',
+#             'PORT' : ''
+
+#         }
+
+#     }
 
 
 # Password validation
@@ -169,7 +169,7 @@ USE_TZ = True
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
+
 STATIC_ROOT = '/vol/web/static'
 MEDIA_ROOT = '/vol/web/media'
 

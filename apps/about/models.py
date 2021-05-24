3aa185ca7c9d97192aed1b9b0cf29d92111a1bd4
Jay Prakash <unijay12@gmail.com>
2021-05-24 20:08:26 +0530

    team section updated

--- apps/about/models.py
+++ apps/about/models.py
@@ -19,12 +19,14 @@ DOMAIN_CHOICE = (
 # Create your models here.
 
 class Team(models.Model):
-    photo = models.ImageField(upload_to='img/team',null=True)
+    photo = models.ImageField(upload_to='about/team',null=True)
     name = models.CharField(max_length=50,null=True)
     branch = models.CharField(max_length=50,null=True)
     domain_assign  = models.CharField(choices=DOMAIN_CHOICE,max_length=2,null=True)
     post_assign = models.CharField(choices=POST_CHOICES,max_length=2,null=True)
-    fb_id = models.URLField(null=True)
+    fb_url = models.URLField(null=True)
+    instagram_url = models.URLField(null=True)
+    github_url = models.URLField(null=True)
     email_id = models.EmailField(null=True)
     phone = models.CharField(max_length=100,null=True)
     joining = models.DateField(auto_created=False,null=True)

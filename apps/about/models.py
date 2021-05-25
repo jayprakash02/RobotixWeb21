3409742dce00ab80bca221a72fdf240ecdbe70a5
D Gopall Krishna <nikhil0223@gmail.com>
2021-05-25 12:35:34 +0530

    Correcting on domain_assign and post_assign

--- apps/about/models.py
+++ apps/about/models.py
@@ -6,14 +6,15 @@ POST_CHOICES = (
     ("HC","HeadCoordinator"),
     ("MM","Manager"),
     ("CO","Coordinator"),
+    ("VO","Volunteer"),
 )
 
 DOMAIN_CHOICE = (
     ("WA","WEB & APP"),
     ("PM","PR MARKETING"),
-    ("DD","DOCUMENTATION"),
+    ("DO","DOCUMENTATION"),
     ("SS","SPONSERSHIP"),
-    ("DD","DESIGN"),
+    ("DV","DESIGN"),
     ("TT","TECHNICAL"),
 )
 # Create your models here.
@@ -22,7 +23,7 @@ class Team(models.Model):
     photo = models.ImageField(upload_to='about/team',null=True)
     name = models.CharField(max_length=250,null=True)
     branch = models.CharField(max_length=250,null=True)
-    domain_assign  = models.CharField(choices=DOMAIN_CHOICE,max_length=50,null=True)
+    domain_assign  = models.CharField(choices=DOMAIN_CHOICE,max_length=2,null=True)
     post_assign = models.CharField(choices=POST_CHOICES,max_length=50,null=True)
     fb_url = models.URLField(blank=True, null=True)
     instagram_url = models.URLField(blank=True, null=True)
--- apps/about/views.py
+++ apps/about/views.py
@@ -16,13 +16,15 @@ class About(APIView):
         Manager = Team.objects.filter(post_assign="MM").all()
         HeadCoordinator = Team.objects.filter(post_assign="HC").all()
         Coordinator = Team.objects.filter(post_assign="CO").all()
+        Volunteer = Team.objects.filter(post_assign="VO").all()
 
         serializer1 = TeamSerializer(Alumini , many=True)
         serializer2 = TeamSerializer(Convenor, many=True)
         serializer3 = TeamSerializer(Manager,many=True)
         serializer4 = TeamSerializer(HeadCoordinator,many=True)
         serializer5 = TeamSerializer(Coordinator,many=True)
+        serializer6 = TeamSerializer(Volunteer,many=True)
 
-        dict = {'Alumini' : serializer1.data ,'Convenor' : serializer2.data ,'Manager' : serializer3.data, 'HeadCoordinator' : serializer4.data, 'Coordinator' : serializer5.data }
+        dict = {'Alumini' : serializer1.data ,'Convenor' : serializer2.data ,'Manager' : serializer3.data, 'HeadCoordinator' : serializer4.data, 'Coordinator' : serializer5.data, 'Volunteer' : serializer6.data }
 
         return Response(dict)

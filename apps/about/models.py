0f66541d998cf22683476ffdfea6661fe7c1d11a
D Gopall Krishna <nikhil0223@gmail.com>
2021-05-25 10:51:07 +0530

    Little tweak to the teams part in about app (to make it work for the api...can later revert it)

--- apps/about/models.py
+++ apps/about/models.py
@@ -20,16 +20,16 @@ DOMAIN_CHOICE = (
 
 class Team(models.Model):
     photo = models.ImageField(upload_to='about/team',null=True)
-    name = models.CharField(max_length=50,null=True)
-    branch = models.CharField(max_length=50,null=True)
-    domain_assign  = models.CharField(choices=DOMAIN_CHOICE,max_length=2,null=True)
-    post_assign = models.CharField(choices=POST_CHOICES,max_length=2,null=True)
-    fb_url = models.URLField(null=True)
-    instagram_url = models.URLField(null=True)
-    github_url = models.URLField(null=True)
-    email_id = models.EmailField(null=True)
-    phone = models.CharField(max_length=100,null=True)
-    joining = models.DateField(auto_created=False,null=True)
+    name = models.CharField(max_length=250,null=True)
+    branch = models.CharField(max_length=250,null=True)
+    domain_assign  = models.CharField(choices=DOMAIN_CHOICE,max_length=50,null=True)
+    post_assign = models.CharField(choices=POST_CHOICES,max_length=50,null=True)
+    fb_url = models.URLField(blank=True, null=True)
+    instagram_url = models.URLField(blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)
+    email_id = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    joining = models.DateField(auto_created=False, blank=True, null=True)
 
 class Achievements(models.Model):
 

30e80b5fe19caf2a0d389902b0d3116b12cd31b1
Jay Prakash <unijay12@gmail.com>
2021-06-08 23:45:16 +0530

    Question Rendering Updated as per domain

--- apps/recruitment/urls.py
+++ apps/recruitment/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 # from .views import FormResponsesViewset, QuestionsViewset, FormResponsesAPIView
 from .views import CandidateFormResponsesAPIView, QuestionsView
-
+router = routers.DefaultRouter()
 
 app_name = 'recruitment'
 
 urlpatterns = [
+    path('',include(router.urls)),
     path('questions/', QuestionsView.as_view()), 
-    path('', CandidateFormResponsesAPIView.as_view())
-]   
+    path('response/', CandidateFormResponsesAPIView.as_view())
+]
--- apps/recruitment/views.py
+++ apps/recruitment/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions, generics
+from rest_framework import serializers, viewsets, permissions, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 
 
 from .serializers import QuestionsSerializer, FormResponsesSerializer, SubmittedUserSerializer
@@ -10,43 +12,32 @@ from .models import Questions, FormResponses, SubmittedUser
 
 
 class QuestionsView(APIView):
-    queryset_openall = Questions.objects.filter(question_for_domain="All")
-    queryset_web = Questions.objects.filter(question_for_domain="Web")
-    queryset_design = Questions.objects.filter(question_for_domain="Design")
-    queryset_pr = Questions.objects.filter(question_for_domain="PR")
-    queryset_core = Questions.objects.filter(question_for_domain="Core")
-    queryset_docs = Questions.objects.filter(question_for_domain="Docs")
-
+    queryset = Questions.objects.all()
+    serializer_class = QuestionsSerializer
     def get(self, request, *args, **kwargs):
-
-        serializer_class_openall = QuestionsSerializer(self.queryset_openall, many=True)
-        serializer_class_web = QuestionsSerializer(self.queryset_web, many=True)
-        serializer_class_design = QuestionsSerializer(self.queryset_design, many=True)
-        serializer_class_pr = QuestionsSerializer(self.queryset_pr, many=True)
-        serializer_class_core = QuestionsSerializer(self.queryset_core, many=True)
-        serializer_class_docs = QuestionsSerializer(self.queryset_docs, many=True)
-
-        hashy = {
-            'openall': serializer_class_openall.data,
-            'web': serializer_class_web.data,
-            'design': serializer_class_design.data,
-            'pr': serializer_class_pr.data,
-            'core': serializer_class_core.data,
-            'docs':serializer_class_docs.data
-        }
-        return Response(hashy)
+        if self.request.data.__contains__("Domain"):
+            question_dic = {}
+            for i in self.request.data["Domain"]:
+                question_instance = self.queryset.filter(
+                    question_for_domain=i)
+                serializer_ = self.serializer_class(question_instance, many=True)
+                question_dic[i]=serializer_.data
+            return Response(question_dic, status=status.HTTP_200_OK)
+        else:
+            return Response("Choose Your Domain",status=status.HTTP_400_BAD_REQUEST)
 
 
 
 class CandidateFormResponsesAPIView(APIView):
     queryset1 = SubmittedUser.objects.all()
     queryset2 = FormResponses.objects.all()
-    
-    def get(self,request,*args, **kwargs):
+
+    def get(self, request, *args, **kwargs):
 
         serializer_class1 = SubmittedUserSerializer(self.queryset1, many=True)
         serializer_class2 = FormResponsesSerializer(self.queryset2, many=True)
 
-        candidate_dict = {'CandidateData' : serializer_class1.data ,'CandidateResponses' : serializer_class2.data}
+        candidate_dict = {'CandidateData': serializer_class1.data,
+                          'CandidateResponses': serializer_class2.data}
 
         return Response(candidate_dict)

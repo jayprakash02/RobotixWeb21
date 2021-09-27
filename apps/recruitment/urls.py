f626feb09c28abf8d3cab05f6ff8be2a568bbf52
Jay Prakash <unijay12@gmail.com>
2021-09-27 19:38:12 +0530

    post request success

--- apps/recruitment/urls.py
+++ apps/recruitment/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 # from .views import FormResponsesViewset, QuestionsViewset, FormResponsesAPIView
-from .views import FormResponses
+from .views import FormResponses, FormResponsesApi
 router = routers.DefaultRouter()
 
 app_name = 'recruitment'
@@ -9,5 +9,5 @@ app_name = 'recruitment'
 urlpatterns = [
     path('',include(router.urls)),
     # path('questions/', QuestionsView.as_view()), 
-    path('response/', FormResponses.as_view())
+    path('response/', FormResponsesApi.as_view())
 ]

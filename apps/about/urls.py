453a51232e35464f4a16c864c620ccb60f527fc0
Pratham Gandhi <ppg.1382@gmail.com>
2021-11-25 20:51:30 +0530

    Added the OTP functionality for making changes to the pfp and adding other team info

--- apps/about/urls.py
+++ apps/about/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path,include
+from django.urls import path, include
 from .views import *
 from rest_framework import routers
 from .serializers import AchievementViewSet
@@ -8,7 +8,8 @@ router = routers.DefaultRouter()
 router.register(r'achievement', AchievementViewSet)
 
 urlpatterns = [
-    path('team/',TeamApi.as_view(),name='text'),
-    path('alumini/',Alumini.as_view(),name='text'),
-    path('',include(router.urls),name='about_api')
+    path('team/', TeamApi.as_view(), name='text'),
+    path('alumini/', Alumini.as_view(), name='text'),
+    path('verify/', VerifyEmail.as_view(), name='text'),
+    path('', include(router.urls), name='about_api')
 ]

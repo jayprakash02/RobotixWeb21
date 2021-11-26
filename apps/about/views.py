46e39e3b8e237e16ca3a81a43dd56f8709e178f8
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2021-11-26 20:33:37 +0530

    Update views.py

--- apps/about/views.py
+++ apps/about/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import *
 from .serializers import *
@@ -44,10 +45,10 @@ class Alumini(APIView):
 class VerifyEmail(APIView):
 
     def post(self, request, *args, **kwargs):
-        email_add = request.POST.get('email')
-        otp = ''.join(secrets.choice(string.ascii_uppercase +
-                      string.digits) for i in range(6))
-        send_mail('OTP for information update', 'Your OTP for information update is ' + otp +
-                  '\nThis OTP is valid for the next 5 minutes', settings.EMAIL_HOST_USER, [email_add], fail_silently=False)
-        hs = hashlib.sha256(otp.encode('utf-8'))
-        return Response(hs.hexdigest())
+        if request.data.__contains__('email'):
+            email_add =request.data['email']
+            otp = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(6))
+            send_mail('OTP for your information update', 'Your OTP for information update is ' + otp +'\nThis OTP is valid for the next 5 minutes', settings.EMAIL_HOST_USER, [email_add], fail_silently=False)
+            hs = hashlib.sha256(otp.encode('utf-8'))
+            return Response(hs.hexdigest(),status=status.HTTP_201_CREATED)
+        return Response('Email Not Provided',status=status.HTTP_400_BAD_REQUEST)
